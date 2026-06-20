b7e956140aa5d52c455108223579ac9c78c36940
Thakur-KD <krrishdeepsingh.1012@gmail.com>
2026-06-20 10:42:33 +0530

    Rename showContent function to reveal

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function showContent() {
+function reveal() {
     document.getElementById('envelope-screen').style.display = 'none';
     document.getElementById('main-content').classList.remove('hidden');
 }
